bc878a064208146cf17f8d182435db9eb8081340
Anas-hammoudeh <Hazhooz123#>
2022-12-24 16:34:42 +0300

    create note model, add and init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/features/home/data/note_model.dart';
 
+import 'core/utils/constants.dart';
 import 'core/utils/theme.dart';
 import 'features/home/presentation/views/notes_view/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
@@ -16,7 +22,6 @@ class NotesApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: darkTheme,
-
       home: const NotesView(),
     );
   }
